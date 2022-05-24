8e8a0a7607c880d21a0e04b016df644089e8227e
Vince14Genius <vince14genius@icloud.com>
2022-05-24 16:27:46 -0700

    improved readability

--- IPA Keyboard Extension/KeyboardViewController+UICollectionViewDataSource.swift
+++ IPA Keyboard Extension/KeyboardViewController+UICollectionViewDataSource.swift
@@ -16,6 +16,7 @@ extension KeyboardViewController: UICollectionViewDataSource {
         return IPASymbols.enabledSections.count
     }
     
+    // numberOfItemsInSection:
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if let numberOfItems = IPASymbols.getKeySet(section: section)?.count {
             return numberOfItems
@@ -24,6 +25,7 @@ extension KeyboardViewController: UICollectionViewDataSource {
         }
     }
     
+    // cellForItemAt:
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewConstants.reuseIdentifier, for: indexPath) as! KeyButtonCell
         cell.delegate.title = IPASymbols.getKeySet(section: indexPath.section)?[indexPath.item]
@@ -31,6 +33,7 @@ extension KeyboardViewController: UICollectionViewDataSource {
         return cell
     }
     
+    // insetForSectionAt:
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(
             top: Layout.topInset,
@@ -40,20 +43,24 @@ extension KeyboardViewController: UICollectionViewDataSource {
         )
     }
     
+    // minimumLineSpacingForSectionAt:
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return Layout.minimumLineSpacing
     }
     
+    // minimumInteritemSpacingForSectionAt:
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return Layout.minimumInteritemSpacing
     }
     
+    // sizeForItemAt:
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let marginsAndInsets = Layout.topInset + Layout.bottomInset + collectionView.safeAreaInsets.top + collectionView.safeAreaInsets.bottom + Layout.minimumInteritemSpacing * CGFloat(Layout.cellsPerColumn - 1)
         let itemHeight = ((collectionView.bounds.size.height - marginsAndInsets) / CGFloat(Layout.cellsPerColumn)).rounded(.down)
         return CGSize(width: itemHeight, height: itemHeight)
     }
     
+    // viewForSupplementaryElementOfKind:at:
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let element = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: CollectionViewConstants.reuseIdentifier, for: indexPath)
         if let header = element as? SectionHeader {
@@ -67,6 +74,7 @@ extension KeyboardViewController: UICollectionViewDataSource {
         return element
     }
     
+    // referenceSizeForHeaderInSection:
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: Layout.getHeaderWidth(keySet: IPASymbols.self, section: section), height: 0)
     }
