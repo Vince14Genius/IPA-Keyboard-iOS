2eb4d8537c591350198a6b2904d4511657f53ea5
Vince14Genius <vince14genius@icloud.com>
2023-08-11 22:01:03 -0400

    added toolbar menu and angle brackets key

--- Core Keyboard Functionality/View Elements/Rows/Button Groups/PinnedSymbolKeys.swift
+++ Core Keyboard Functionality/View Elements/Rows/Button Groups/PinnedSymbolKeys.swift
@@ -10,20 +10,60 @@ import SwiftUI
 
 struct PinnedSymbolKeys: View {
     weak var inputViewController: UIInputViewController?
+    let keyboardSizeClass: KeyboardSizeClass
     
-    var body: some View {
-        HStack(spacing: 0) {
+    var nonfoldable: some View {
+        Group {
             Button("[ ]") {
                 inputViewController?.typeSandwich(Symbols.squareBrackets)
             }
             Button("/ /") {
                 inputViewController?.typeSandwich(Symbols.forwardSlashes)
             }
+        }
+    }
+    
+    var foldableExtraCrowded: some View {
+        Group {
             Button("~") {
                 inputViewController?.type(text: Symbols.tilde)
             }
+        }
+    }
+    
+    var foldableNonWide: some View {
+        Group {
             Button("◌") {
                 inputViewController?.type(text: Symbols.dottedCircle)
+            }
+            Button("⟨ ⟩") {
+                inputViewController?.typeSandwich(Symbols.angleBrackets)
+            }
+        }
+    }
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            nonfoldable
+            if !keyboardSizeClass.isExtraCrowded {
+                foldableExtraCrowded
+            }
+            if keyboardSizeClass.isWide {
+                foldableNonWide
+            } else {
+                Menu {
+                    if keyboardSizeClass.isExtraCrowded {
+                        foldableExtraCrowded
+                    }
+                    foldableNonWide
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                        .offset(y: 2)
+                }
+                .foregroundColor(Color(.label))
+                .padding([.top, .bottom])
+                .padding([.leading, .trailing], Layout.leftInsetRaw)
+                .background(Color.clearInteractable)
             }
         }
         .buttonStyle(ToolbarButtonStyle())
